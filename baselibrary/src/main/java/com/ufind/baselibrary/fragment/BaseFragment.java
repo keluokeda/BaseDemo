e6d498592353e7d66d138a128dcaa7e4af0a8a90
yuyanhui <913918146@qq.com>
2017-03-02 14:48:12 +0800

    jitpack

--- baselibrary/src/main/java/com/ufind/baselibrary/fragment/BaseFragment.java
+++ baselibrary/src/main/java/com/ufind/baselibrary/fragment/BaseFragment.java
@@ -1,14 +1,20 @@
 package com.ufind.baselibrary.fragment;
 
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.ufind.baselibrary.R;
 
@@ -22,6 +28,7 @@ public abstract class BaseFragment extends Fragment {
     private View customErrorView;
 
     private FrameLayout mRootView;
+    private ProgressDialog mProgressDialog;
 
     public void addSubscription(Subscription subscription) {
         mCompositeSubscription.add(subscription);
@@ -157,6 +164,51 @@ public abstract class BaseFragment extends Fragment {
      * 开始刷新视图内容
      */
     public abstract void beginRefresh();
+
+
+
+    protected final void showProgressDialog(CharSequence title) {
+        if (mProgressDialog == null) {
+            initProgressDialog();
+        }
+        mProgressDialog.setMessage(title);
+        mProgressDialog.show();
+    }
+
+    protected final void dismissProgressDialog() {
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    private void initProgressDialog() {
+        mProgressDialog = new ProgressDialog(getContext());
+    }
+
+    protected final void showSnackBar(View view, CharSequence content) {
+        Snackbar.make(view, content, Snackbar.LENGTH_SHORT).show();
+    }
+
+    public final void showSnackBar(String contentText, String actionText, final Runnable runnable) {
+        Snackbar mSnackbar = Snackbar.make(getActivity().getWindow().getDecorView(), contentText, Snackbar.LENGTH_SHORT);
+        mSnackbar.setAction(actionText, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runnable.run();
+            }
+        });
+        mSnackbar.setActionTextColor(Color.RED);
+        //设置文字颜色为白色
+        ((TextView) mSnackbar.getView().findViewById(android.support.design.R.id.snackbar_text)).setTextColor(Color.WHITE);
+        mSnackbar.show();
+    }
+
+    protected final void hideSoftKeyBoard() {
+        if (getActivity() != null) {
+            InputMethodManager manager = (InputMethodManager) this.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            manager.hideSoftInputFromWindow(getActivity().getWindow().getDecorView().getWindowToken(), 0);//隐藏软键盘
+        }
+    }
 
 
 }
